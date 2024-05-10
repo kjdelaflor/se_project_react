9abfe6a21bd11608e249a82bff88b2921252ed9e
JUSTIN DELA FLOR <KJDELAFLOR@GMAIL.COM>
2024-05-09 19:51:04 -0700

    early stages 1

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.jpg";
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-and-location">DATE, LOCATION</p>
+      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrance Tegegne</p>
+        <img src={avatar} alt="Terrance Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+export default Header;
